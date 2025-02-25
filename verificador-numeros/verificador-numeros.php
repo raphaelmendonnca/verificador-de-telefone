1a4ad43230ba2fd51dcde3153788e510b54a545e
Raphael Mendonça <57078626+raphaelmendonnca@users.noreply.github.com>
2025-02-25 18:34:40 -0300

    Update verificador-numeros.php
    Adicionado mensagem de número já existente

--- verificador-numeros/verificador-numeros.php
+++ verificador-numeros/verificador-numeros.php
@@ -409,7 +409,7 @@ class VerificadorNumeros {
     }
 }
 
-// Funções AJAX do admin
+// Função AJAX do admin modificada
 add_action('wp_ajax_add_numero', 'admin_add_numero');
 function admin_add_numero() {
     check_ajax_referer('add_numero_action', 'nonce');
@@ -418,29 +418,27 @@ function admin_add_numero() {
     }
 
     $numero = sanitize_text_field($_POST['numero']);
+    $numero_limpo = preg_replace('/[^0-9]/', '', $numero);
+    
+    // Verificar se o número já existe antes de tentar inserir
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'verificador_numeros';
+    $exists = $wpdb->get_var($wpdb->prepare(
+        "SELECT COUNT(*) FROM {$table_name} WHERE numero = %s",
+        $numero_limpo
+    ));
+    
+    if ($exists) {
+        wp_send_json_error(array('message' => 'Este número já está cadastrado na base de dados.'));
+        return;
+    }
+    
     $instance = VerificadorNumeros::get_instance();
     
-    if ($instance->adicionar_numero($numero)) {
+    if ($instance->adicionar_numero($numero_limpo)) {
         wp_send_json_success();
     } else {
         wp_send_json_error(array('message' => 'Erro ao adicionar número.'));
-    }
-}
-
-add_action('wp_ajax_delete_numero', 'admin_delete_numero');
-function admin_delete_numero() {
-    check_ajax_referer('delete_numero_nonce', 'nonce');
-    if (!current_user_can('manage_options')) {
-        wp_send_json_error(array('message' => 'Permissão negada.'));
-    }
-
-    $id = intval($_POST['id']);
-    $instance = VerificadorNumeros::get_instance();
-    
-    if ($instance->excluir_numero($id)) {
-        wp_send_json_success();
-    } else {
-        wp_send_json_error(array('message' => 'Erro ao excluir número.'));
     }
 }
 
